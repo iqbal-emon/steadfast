45e90239006733b0c425efc26a864b44a958b2d2
iqbal-emon <ihemon281@gmail.com>
2024-10-07 00:52:27 +0600

    Done in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "../components/nav-bar/nav-bar.component";
+import { HeroComponent } from "../components/hero/hero.component";
+import { CardComponent } from "../components/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HeroComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
